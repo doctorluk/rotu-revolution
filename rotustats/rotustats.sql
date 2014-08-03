509dc9154023375b8960ab143b9fa94d6e8a21e3
Lukas Monka <lukgit@gmail.com>
2014-08-03 22:58:27 +0200

    Fixed bad auto_increment value in default sql

--- rotustats/rotustats.sql
+++ rotustats/rotustats.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `rotustats_game` (
   `port` smallint(8) unsigned NOT NULL,
   `date` datetime NOT NULL,
   KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=48 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
